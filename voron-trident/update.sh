a20d006ca8b6a7659b873faf242c0b03acbcea0e
Lars Bernau <lars.bernau@priara.de>
2022-08-05 11:11:47 +0200

    Added Can0 to update script

--- voron-trident/update.sh
+++ voron-trident/update.sh
@@ -10,11 +10,10 @@ read -p "Octopus firmware built, please check above for any errors. Press [Enter
 make flash KCONFIG_CONFIG=config.octopus FLASH_DEVICE=/dev/serial/by-id/usb-Klipper_stm32f446xx_33005D001551303432323631-if00
 read -p "Octopus firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 
-# make clean KCONFIG_CONFIG=config.sht42
-# make menuconfig KCONFIG_CONFIG=config.sht42
-
-# make KCONFIG_CONFIG=config.sht42
-# read -p "SHT 42 (CAN-Board) firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
-# make flash KCONFIG_CONFIG=config.sht42
+make clean KCONFIG_CONFIG=config.sht42
+make menuconfig KCONFIG_CONFIG=config.sht42
+make KCONFIG_CONFIG=config.sht42
+read -p "SHT 42 (CAN-Board) firmware built, please check above for any errors. Press [Enter] to continue flashing, or [Ctrl+C] to abort"
+# python3 ~/CanBoot/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u MYUUID
 
 sudo service klipper start
