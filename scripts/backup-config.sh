318ec45c89747307b56e1156bae5b7eadf4f7ae8
Lars Bernau <lbernau@users.noreply.github.com>
2022-08-24 13:51:01 +0200

    Backup triggered on 2022-08-24 13:51:01

---
+++ scripts/backup-config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+### Path to your config folder you want to backup
+config_folder=~/klipper_config
+
+push_config(){
+  cd $config_folder
+  echo Pushing updates
+  git pull -v
+  git add . -v
+  current_date=$(date +"%Y-%m-%d %T")
+  git commit -m "Backup triggered on $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
+  git push
+}
+
+push_config
